df9e5e41014825b829e5d941b89f9d6fbe4d22d4
andrey kovyarov <andrey.kovyarov@gmail.com>
2024-09-26 18:47:34 +0300

    added tailwind base styles

--- services/frontend/vite.config.ts
+++ services/frontend/vite.config.ts
@@ -4,6 +4,8 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  clearScreen: false,
+
   plugins: [vue({
     ...templateCompilerOptions,
   })],
