21b8d5e0c108516e5941182640f4f9cf811c4af7
andrey kovyarov <andrey.kovyarov@gmail.com>
2024-09-29 17:08:04 +0300

    refactored app component

--- services/frontend/src/App.vue
+++ services/frontend/src/App.vue
@@ -1,42 +1,32 @@
 <script setup lang="ts">
-import { OrbitControls, Sky } from '@tresjs/cientos';
+import { Sky } from '@tresjs/cientos';
 import { TresCanvas } from '@tresjs/core';
+import Camera from './components/Camera.vue';
 import Playground from './components/Playground.vue';
+import SceneSettings from './components/SceneSettings.vue';
 </script>
 
 <template>
-  <TresCanvas shadows window-size render-mode="on-demand">
-    <TresPerspectiveCamera :position="[5, 7.5, 7.5]" />
+  <TresCanvas shadows window-size render-mode="manual">
+    <Camera />
 
-    <OrbitControls />
     <Sky
       :turbidity="10.4"
       :rayleigh="1.837"
       :mie-coefficient="0.063"
       :mie-directional-g="0.975"
       :elevation="24.7"
+      :azimuth="180"
       :exposure="0.324"
     />
 
-    <TresAmbientLight :intensity="1" />
-    <TresDirectionalLight :position="[5, 5, 2]" :intensity="1" />
+    <TresDirectionalLight :position="[50, 50, 20]" :intensity="5" />
+    <TresDirectionalLight :position="[-30, 3, -50]" :intensity="5" />
 
     <Suspense>
       <Playground />
     </Suspense>
-
-    <TresMesh
-      :position="[-2, 2, 0]"
-      :rotation="[0, Math.PI, 0]"
-    >
-      <TresConeGeometry :args="[1, 1.5, 3]" />
-      <TresMeshToonMaterial color="#82DBC5" />
-    </TresMesh>
-    <TresMesh
-      :position="[0, 0, 0]"
-    >
-      <TresBoxGeometry :args="[1.5, 1.5, 1.5]" />
-      <TresMeshToonMaterial color="#4F4F4F" />
-    </TresMesh>
   </TresCanvas>
+
+  <SceneSettings />
 </template>
