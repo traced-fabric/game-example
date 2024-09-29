7312990c18895fe72f1799d99bfbbc9a82f440df
andrey kovyarov <andrey.kovyarov@gmail.com>
2024-09-29 17:07:36 +0300

    refactor playground component to dynamically update room color based on scene settings

--- services/frontend/src/components/Playground.vue
+++ services/frontend/src/components/Playground.vue
@@ -1,10 +1,21 @@
 <script lang="ts" setup>
-import { useLoader } from '@tresjs/core';
+import { useLoader, useTres } from '@tresjs/core';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader';
+import { watch } from 'vue';
+import { useSceneSettingsStore } from '../stores/sceneSettings';
 
-const { scene: playground } = await useLoader(GLTFLoader, '../public/models/playground/playground.glb');
+const model = await useLoader(GLTFLoader, '/models/playground/playground.glb');
+
+const sceneSettings = useSceneSettingsStore();
+const { advance } = useTres();
+
+model.nodes.Room.material.color.set(sceneSettings.room.color);
+watch(() => sceneSettings.room.color, (color) => {
+  model.nodes.Room.material.color.set(color);
+  advance();
+});
 </script>
 
 <template>
-  <primitive :object="playground" />
+  <primitive :object="model.scene" />
 </template>
