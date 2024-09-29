c9a20ac196d58799b5d4d6d3c72d50c615fc7adc
andrey kovyarov <andrey.kovyarov@gmail.com>
2024-09-29 17:06:49 +0300

    added camera component with OrbitControls

---
+++ services/frontend/src/components/Camera.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+import { OrbitControls } from '@tresjs/cientos';
+import { useTres } from '@tresjs/core';
+
+const { advance } = useTres();
+
+function render() {
+  advance();
+}
+</script>
+
+<template>
+  <TresPerspectiveCamera :position="[30, 20, 10]" :look-at="[0, 8, 0]" />
+  <OrbitControls @change="render" />
+</template>
